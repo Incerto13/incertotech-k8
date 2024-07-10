28e1dbdb13084ed3873c3fd602ce4836921ed4e1
Hezekiah <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-07-10 07:41:18 -0400

    add run docker script

---
+++ run-docker.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+# expand homepage links and generate final index.html file
+bash expand-homepage-links.sh homepage/index-no-links.html homepage/index.html
+
+
+source .env
+docker-compose up --build -d
+
