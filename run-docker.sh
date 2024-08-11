999ea5f6542841ae7999bef90bd25623940fd922
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-08-11 13:44:14 -0400

    add letsencrypt

--- run-docker.sh
+++ run-docker.sh
@@ -1,9 +1,4 @@
 # !/bin/bash
 
-# expand homepage links and generate final index.html file
-# bash expand-homepage-links.sh homepage/index-no-links.html homepage/index.html
-
 source .env
 docker compose build --no-cache && docker compose up --force-recreate -d
-
-
