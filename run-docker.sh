ebd92e7d9a825d80390b2ba85a6303862f8874fd
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-08-11 15:04:18 -0400

    fix certbot

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,7 @@
 # !/bin/bash
 
 source .env
+# run certbox container
+docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d staging.incertotech.com
+# run rest of containers
 docker compose build --no-cache && docker compose up --force-recreate -d
