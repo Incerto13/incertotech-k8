d7e190f57212d68c9c17852044db5c5247ecd217
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-08-10 22:27:58 -0400

    remove extra invocation of expand-script

--- run-docker.sh
+++ run-docker.sh
@@ -1,8 +1,7 @@
 # !/bin/bash
 
 # expand homepage links and generate final index.html file
-bash expand-homepage-links.sh homepage/index-no-links.html homepage/index.html
-
+# bash expand-homepage-links.sh homepage/index-no-links.html homepage/index.html
 
 source .env
 docker compose build --no-cache && docker compose up --force-recreate -d
