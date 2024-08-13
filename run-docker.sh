22c290bf44ef850ec09d6028d0020ff572b259d4
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-08-13 04:47:06 -0400

    add letsencrypt init script

--- run-docker.sh
+++ run-docker.sh
@@ -3,5 +3,9 @@
 source .env
 # run certbox container
 # docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d staging.incertotech.com
-# run rest of containers
-# docker compose build --no-cache && docker compose up --force-recreate -d
+
+# run letsencrypt init script
+ bash ./init-letsencrypt.sh
+
+# run server and apps
+docker compose build --no-cache && docker compose up --force-recreate -d
