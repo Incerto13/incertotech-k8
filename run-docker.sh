fa67fcbb95514d611652ccc8f79e9bca98a26677
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-08-13 19:53:22 -0400

    move certbot into differnt compose file

--- run-docker.sh
+++ run-docker.sh
@@ -1,8 +1,6 @@
 # !/bin/bash
 
 source .env
-# run certbox container
-# docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d staging.incertotech.com
 
 # run server and apps
-# docker compose build --no-cache && docker compose up --force-recreate -d
+docker compose build --no-cache && docker compose up --force-recreate -d
