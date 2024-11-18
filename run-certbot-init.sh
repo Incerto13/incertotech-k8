63cb20d08060d16e02d3eb4c57da4b4d1d12c3bb
incerto13 <hezekiah@hezekiahs-mbp.myfiosgateway.com>
2024-11-17 19:13:29 -0500

    use sh instead of bash

--- run-certbot-init.sh
+++ run-certbot-init.sh
@@ -2,4 +2,4 @@
 
 docker compose -f docker-compose.ssl.yml up nginx certbot-init -d
 
-docker exec -it certbot-init bash -c "certbot certonly --webroot -w /var/www/certbot --force-renewal --email ${LETSENCRYPT_EMAIL} -d ${BASE_DOMAIN} -d ${REACT_TO_DO_DOMAIN} -d ${REACT_ELECTORAL_MAP_DOMAIN} -d ${REACT_COURSE_ADMIN_DOMAIN} -d ${NEST_TO_DO_API_DOMAIN} -d ${NEST_BLOG_API_DOMAIN} -d ${NEST_COURSE_ADMIN_API_DOMAIN} --agree-tos"
+docker exec -it certbot-init sh -c "certbot certonly --webroot -w /var/www/certbot --force-renewal --email ${LETSENCRYPT_EMAIL} -d ${BASE_DOMAIN} -d ${REACT_TO_DO_DOMAIN} -d ${REACT_ELECTORAL_MAP_DOMAIN} -d ${REACT_COURSE_ADMIN_DOMAIN} -d ${NEST_TO_DO_API_DOMAIN} -d ${NEST_BLOG_API_DOMAIN} -d ${NEST_COURSE_ADMIN_API_DOMAIN} --agree-tos"
